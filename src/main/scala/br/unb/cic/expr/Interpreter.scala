9eda92e861da4436e255d4d44488f6f67a3fff2f
Mattelis <76789267+Mattelis@users.noreply.github.com>
2024-08-04 16:28:22 -0300

    Update Interpreter.scala

--- src/main/scala/br/unb/cic/expr/Interpreter.scala
+++ src/main/scala/br/unb/cic/expr/Interpreter.scala
@@ -1,32 +1,33 @@
 package br.unb.cic.flang
 
+import MErrState._
+
 import Declarations._
-import StateMonad._
+import Substitution._
+import cats.data.EitherT
 import cats.data.State
 
 object Interpreter {
-
-  def eval(expr: Expr, declarations: List[FDeclaration]): MState[Integer] =
-    expr match {
-      case CInt(v) => pure(v)
-      case Add(lhs, rhs) => for {
-        l <- eval(lhs, declarations)
-        r <- eval(rhs, declarations)
-      } yield l + r
-      case Mul(lhs, rhs) => for {
-        l <- eval(lhs, declarations)
-        r <- eval(rhs, declarations)
-      } yield l * r
-      case Id(name) => for {
-        state <- State.get[S]
-        res <- pure(lookupVar(name, state))
-      } yield res
-      case App(name, arg) => for {
-        fdecl <- lookup(name, declarations)
-        value <- eval(arg, declarations)
-        s1 <- State.get[S]
-        s2 <- State.set(declareVar(fdecl.arg, value, s1))
-        res <- eval(fdecl.body, declarations)
-      } yield res
-    }
+  def eval(expr: Expr, declarations: List[FDeclaration]): MStateEH[Integer] = expr match {
+    case CInt(v) => pure(v)
+    case Add(lhs, rhs) => for {
+      l <- eval(lhs, declarations)
+      r <- eval(rhs, declarations)
+    } yield l + r
+    case Mul(lhs, rhs) => for {
+      l <- eval(lhs, declarations)
+      r <- eval(rhs, declarations)
+    } yield l * r
+    case Id(name) => for {
+      state <- EitherT.right(State.get[S])
+      res <- lookupVar(name, state)
+    } yield res
+    case App(name, arg) => for {
+      fdecl <- lookup(name, declarations)
+      value <- eval(arg, declarations) 
+      s1 <- EitherT.right(State.get[S])
+      s2 <- EitherT.right(State.set(declareVar(fdecl.arg, value, s1)))
+      res <- eval(fdecl.body, declarations)
+    } yield res
+  }
 }
