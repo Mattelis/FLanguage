2b335e17f9fe88b5af326213d37fbf9600aa194d
Mattelis <76789267+Mattelis@users.noreply.github.com>
2024-08-04 15:19:22 -0300

    Update InterpreterTest.scala

--- src/test/scala/br/unb/cic/expr/InterpreterTest.scala
+++ src/test/scala/br/unb/cic/expr/InterpreterTest.scala
@@ -1,34 +1,60 @@
-package br.unb.cic.expr
+package br.unb.cic.flang
 
 import org.scalatest._
 import flatspec._
 import matchers._
+
 import Interpreter._
+import Declarations._
+import StateMonad._
+import cats.data.State
+
 class InterpreterTest extends AnyFlatSpec with should.Matchers {
+
+  val inc = FDeclaration("inc", "x", Add(Id("x"), CInt(1)))
+
+  val declarations = List(inc)
+
+  val initialState: S = List()
 
   "eval CInt(5)" should "return an integer value 5." in {
     val c5 = CInt(5)
-    eval(c5) should be (5)
+    val state = eval(c5, declarations)
+    val (_, res) = state.run(initialState).value
+    res should be (5)
   }
 
   "eval Add(CInt(5), CInt(10)) " should "return an integer value 15." in {
     val c5  = CInt(5)
     val c10 = CInt(10)
     val add = Add(c5, c10)
-    eval(add) should be (15)
+    val state = eval(add, declarations)
+    val (_, res) = state.run(initialState).value
+    res should be (15)
   }
 
   "eval Add(CInt(5), Add(CInt(5), CInt(10))) " should "return an integer value 20." in {
     val c5 = CInt(5)
     val c10 = CInt(10)
     val add = Add(c5, Add(c5, c10))
-    eval(add) should be(20)
+    val state = eval(add, declarations)
+    val (_, res) = state.run(initialState).value
+    res should be(20)
   }
 
   "eval Mul(CInt(5), CInt(10))" should "return an integer value 50" in {
     val c5 = CInt(5)
     val c10 = CInt(10)
     val mul = Mul(c5, CInt(10))
-    eval(mul) should be(50)
+    val state = eval(mul, declarations)
+    val (_, res) = state.run(initialState).value
+    res should be(50)
+  }
+
+  "eval App(inc, 99) " should "return an integer value 100" in {
+    val app = App("inc", CInt(99))
+    val state = eval(app, declarations)
+    val (_, res) = state.run(initialState).value
+    res should be (100)
   }
 }
