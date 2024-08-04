42026780d6f2e5196c34fa3001c18351b5dfa562
Mattelis <76789267+Mattelis@users.noreply.github.com>
2024-08-04 15:17:35 -0300

    Update Interpreter.scala

--- src/main/scala/br/unb/cic/expr/Interpreter.scala
+++ src/main/scala/br/unb/cic/expr/Interpreter.scala
@@ -1,9 +1,32 @@
-package br.unb.cic.expr
+package br.unb.cic.flang
+
+import Declarations._
+import StateMonad._
+import cats.data.State
 
 object Interpreter {
-  def eval(expr: Expr) : Integer = expr match {
-    case CInt(v) => v
-    case Add(v,u) => eval(v) + eval(u)
-    case Mul(v,u) => eval(v) * eval(u)
-  }
+
+  def eval(expr: Expr, declarations: List[FDeclaration]): MState[Integer] =
+    expr match {
+      case CInt(v) => pure(v)
+      case Add(lhs, rhs) => for {
+        l <- eval(lhs, declarations)
+        r <- eval(rhs, declarations)
+      } yield l + r
+      case Mul(lhs, rhs) => for {
+        l <- eval(lhs, declarations)
+        r <- eval(rhs, declarations)
+      } yield l * r
+      case Id(name) => for {
+        state <- State.get[S]
+        res <- pure(lookupVar(name, state))
+      } yield res
+      case App(name, arg) => for {
+        fdecl <- lookup(name, declarations)
+        value <- eval(arg, declarations)
+        s1 <- State.get[S]
+        s2 <- State.set(declareVar(fdecl.arg, value, s1))
+        res <- eval(fdecl.body, declarations)
+      } yield res
+    }
 }
